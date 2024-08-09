9080f6da054163d53b66aa0f8c4294853b908a74
Abylkasym uulu Alidin <alidinablylkasymuulu03@gmail.com>
2024-08-09 03:59:42 +0200

    bug fix

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,5 @@
 # app/auth.py
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
@@ -20,16 +20,21 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def authenticate_user(db: Session, username: str, password: str):
     user = crud.get_user_by_username(db, username)
-    if not user or not pwd_context.verify(password, user.hashed_password):
+    if not user:
+        print("User not found")
+        return False
+    if not pwd_context.verify(password, user.hashed_password):
+        print("Password verification failed")
         return False
     return user
+
 
--- app/crud.py
+++ app/crud.py
@@ -23,14 +23,14 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 def create_post(db: Session, post: schemas.PostCreate, user_id: int):
-    db_post = models.Post(**post.dict(), owner_id=user_id)
+    db_post = models.Post(**post.model_dump(), owner_id=user_id)
     db.add(db_post)
     db.commit()
     db.refresh(db_post)
     return db_post
 
 def create_comment(db: Session, comment: schemas.CommentCreate, post_id: int, is_blocked: bool):
-    db_comment = models.Comment(**comment.dict(), post_id=post_id, is_blocked=is_blocked)
+    db_comment = models.Comment(**comment.model_dump(), post_id=post_id, is_blocked=is_blocked)
     db.add(db_comment)
     db.commit()
     db.refresh(db_comment)
